643ebebcc35d7181203412d624002431dcbbb4aa
Gonzalo Lopez <gonzolopez@outlook.com.ar>
2022-08-20 20:38:17 -0300

    Agrego delete a cada item del carrito. Agrego loading en itemDetail

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,7 +1,7 @@
 import './CartWidget.css';
 import { useContext } from 'react';
 import { CartInfoContext } from '../../context/CartInfoContext';
-
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
 
@@ -9,10 +9,11 @@ const CartWidget = () => {
 
     const quantity = getCartInfoCount();
 
+
     return (
         <div className="cart-widget">
             <img src='/images/cart.svg' alt='Shopping Cart' /> 
-            {quantity} items
+            { quantity === 0 ? null : <Link to='/cart'>{quantity} items </Link> }
         </div>
     );
 }
