0d50a3c8605bc1ebe78eef8ebfa1dc5b95449f3b
Gonzalo Lopez <gonzolopez@outlook.com.ar>
2022-08-21 13:45:08 -0300

    Fix del id del item

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,25 +5,20 @@ import { backend } from './firebase'
 const collectionName = 'products';
 
 export const GetCatalogFromApi = async (category) => { 
-    //console.log("entro");
     const collectionRef = !category ? 
         collection( backend, collectionName) 
         : query(collection(backend, collectionName), where('category', '==', category)) 
-    //console.log(collectionRef);
     const ppp = await getDocs(collectionRef);
     const products = ppp.docs.map( doc => {
                 const data = doc.data();
                 return {id: doc.id, ...data};
             });
-            //console.log(products);
             return products;
 }
 
 export const GetCatalogFromApiById = async (id) => { 
-   
     const response = await getDoc(doc(backend, collectionName, id));
     const data = response.data();
-    const product = {id: doc.id, ...data};
-    //console.log(products);
+    const product = {id: response.id, ...data};
     return product;
 }
